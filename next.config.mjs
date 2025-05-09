b12706a515bf1c47bba7b42c89e862343958319f
nathaliejanep <nathalie.persson@hotmail.com>
2025-05-09 11:31:22 +0200

    Add assetprefix docs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   basePath: '/docs',
+  assetPrefix: '/docs',
   trailingSlash: true, // Optional but helps with static exports and routing
   reactStrictMode: true,
 };
